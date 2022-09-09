f2b8468b189c9a52724142c8d8f61e79e461e4a2
Harikant Jammi <harikant_jammi@yahoo.co.in>
2022-09-09 21:24:30 +0530

    added public modifiers

--- Sources/FootballAPIClient/FootballAPIClient+Teams.swift
+++ Sources/FootballAPIClient/FootballAPIClient+Teams.swift
@@ -23,47 +23,47 @@ public extension FootballAPIClient {
             self.request = request
         }
         
-        func id(_ id: String) -> Self {
+        public func id(_ id: String) -> Self {
             queryParams[.id] = id
             return self
         }
         
-        func name(_ id: String) -> Self {
+        public func name(_ id: String) -> Self {
             queryParams[.name] = id
             return self
         }
         
-        func code(_ code: String) -> Self {
+        public func code(_ code: String) -> Self {
             queryParams[.code] = code
             return self
         }
         
-        func search(_ search: String) -> Self {
+        public func search(_ search: String) -> Self {
             queryParams[.search] = search
             return self
         }
         
-        func country(_ country: String) -> Self {
+        public func country(_ country: String) -> Self {
             queryParams[.country] = country
             return self
         }
         
-        func league(_ league: String) -> Self {
+        public func league(_ league: String) -> Self {
             queryParams[.league] = league
             return self
         }
         
-        func season(_ season: String) -> Self {
+        public func season(_ season: String) -> Self {
             queryParams[.season] = season
             return self
         }
         
         
-        func getAsync() async throws -> FootballAPIClient.Response<[FootballAPIClient.Model.TeamInfo]> {
+        public func getAsync() async throws -> FootballAPIClient.Response<[FootballAPIClient.Model.TeamInfo]> {
             try await request.getAsync(path: .teams, queryParams: queryParams)
         }
         
-        func get() -> EventLoopFuture<FootballAPIClient.Response<[FootballAPIClient.Model.TeamInfo]>> {
+        public func get() -> EventLoopFuture<FootballAPIClient.Response<[FootballAPIClient.Model.TeamInfo]>> {
             request.get(path: .teams, queryParams: queryParams)
         }
         
